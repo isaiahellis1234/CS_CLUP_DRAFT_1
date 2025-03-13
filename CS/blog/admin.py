97300c9b83afbcb8320f1a3c78e1bbc18763e00e
Isaiah Ellis <isaiahellis1234@gmail.com>
2025-03-13 14:47:35 -0700

    This is towards the end of the drafgt
    This is currently what the first draft looks like

--- CS/blog/admin.py
+++ CS/blog/admin.py
@@ -1,7 +1,8 @@
 # blog/admin.py
 
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 # Register the Post model to be accessible via Django Admin
 admin.site.register(Post)
+admin.site.register(Comment)
--- CS/blog/views.py
+++ CS/blog/views.py
@@ -35,18 +35,16 @@ def frontpage(request):
     return render(request, "blog/frontpage.html", {"posts": posts, "form": form})
 
 def post_detail(request, slug):
-    post = Post.objects.get(slug=slug)
+    post = get_object_or_404(Post, slug=slug)  # Get the post by its slug
 
     if request.method == "POST":
         form = CommentForm(request.POST)
-
         if form.is_valid():
             comment = form.save(commit=False)
-            comment.post = post
+            comment.post = post  # Associate the comment with the post
             comment.save()
-
-            return redirect("post_detail", slug=post.slug)
+            return redirect('post_detail', slug=post.slug)  # Redirect to avoid resubmission
     else:
         form = CommentForm()
 
-    return render(request, "blog/post_detail.html", {"post": post, "form": form})
+    return render(request, 'blog/post_detail.html', {'post': post, 'form': form})
